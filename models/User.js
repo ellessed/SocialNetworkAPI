d3333ec785efb31627d58c07feb40c1b0831abaf
elleserdonaldson <ellessedon@gmail.com>
2023-03-05 00:31:34 +0000

    initating our user mdel and overrided json default behaviour through virtuals

--- models/User.js
+++ models/User.js
@@ -1,29 +1,47 @@
 //mongoose required
 const { Schema, model } = require("mongoose");
 
-const useSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+\@.+\..+/, "Email address must be matched!"],
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+const useSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Email address must be matched!"],
     },
-  ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // In this case, we're overriding JSON's default behaviour and specifying that virtuals should be included in our response.
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friend-Count").get(function () {
+  return this.friends.length;
 });
+
+// Initialize our User model
+const User = model("User", useSchema);
+
+module.exports = User;
