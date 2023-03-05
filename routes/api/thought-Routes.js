1dca645184c755f1a942f5433e73c060ac939f3d
elleserdonaldson <ellessedon@gmail.com>
2023-03-05 01:03:34 +0000

    user thought routes created to

---
+++ routes/api/thought-Routes.js
@@ -0,0 +1,28 @@
+//express router imported
+
+const router = require("express").Router();
+//import thought and reaction methods
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-Controller");
+
+//routes
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/user-Routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+//import user methods
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addUserFriend,
+  deleteUserFriend,
+} = require("../../controllers/user-Controller");
+
+// routes
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(addUserFriend)
+  .delete(deleteUserFriend);
+
+module.exports = router;
